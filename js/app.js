948ca2fae7d0bd40bf484f064f54d70cd76397da
wendelmaghinang <janwendelmaghinang@gmail.com>
2020-04-06 01:13:09 +0800

    getting news article from Api and getting value of category,country option menu and search bar

---
+++ js/app.js
@@ -0,0 +1,54 @@
+
+
+ let selectionCountry = document.getElementById('selectCountry');
+ let selectionCategory = document.getElementById('selectCategory');
+ let searchQuery = document.getElementById('search');
+ let results = document.getElementById('result');
+
+document.getElementById('searchbtn').addEventListener('click', loadUsers);
+
+function loadUsers(){
+
+  var country = selectionCountry.options[selectionCountry.selectedIndex].value;  
+  var category = selectionCategory.options[selectionCategory.selectedIndex].value;
+
+const URL = 'https://newsapi.org/v2/top-headlines?q='+searchQuery.value+'&apiKey=ff55591e19c74c018563bc1852ae1dde&country='+country+'&category='+category+'&q=&page=1&pageSize=50'
+const news = document.querySelector('.news')
+
+console.log(URL)
+fetch(URL)
+   .then(data => data.json())
+
+   .then(response => {
+      // const totalArticles = response.totalResults;
+       const articles = response.articles
+
+       let newsItem = ''
+       articles.forEach(article => {
+         newsItem += `
+     <div class="news-item">
+             <div class="news-image">
+                  <img src = "${article.urlToImage}"width="300" height="300"></img>
+             </div>
+          <div class="news-title">   
+             <h1> ${article.title} </h1> 
+          </div>
+          <div class="news-desc">
+             <p> ${article.description} </p>   
+          </div>
+          <div class="news-url"> 
+             <a href="${article.url}">Go To Page</a>
+          </div>  
+      </div>`
+       });
+       
+       news.innerHTML = newsItem;
+
+   })
+
+   .catch(error => {
+  
+   })
+ }
+
+
